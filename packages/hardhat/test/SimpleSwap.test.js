7e3bdc412b4e680fd767ece5f8eccc5ed461412c
Alex Zhao <alexzhao.mailbox@gmail.com>
2024-07-08 20:21:16 -0400

    attempted changes to isolate bugs

--- packages/hardhat/test/SimpleSwap.test.js
+++ packages/hardhat/test/SimpleSwap.test.js
@@ -2,8 +2,11 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 // const WETH_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-const WETH_ADDRESS = "0x122013fd7df1c6f636a5bb8f03108e876548b455";
+// const WETH_ADDRESS = "0x122013fd7df1c6f636a5bb8f03108e876548b455";
+const WETH_ADDRESS = "0x2def4285787d58a2f811af24755a8150622f4361";
+const CELO_ADDRESS = "0x471EcE3750Da237f93B8E339c536989b8978a438";
 const DAI_ADDRESS = "0xE4fE50cdD716522A56204352f00AA110F731932d";
+const USDC_ADDRESS = "0xef4229c8c3250c675f21bcefa42f58efbff6002a";
 const DAI_DECIMALS = 18;
 const SwapRouterAddress = "0x5615CDAb10dc425a742d643d949a7F474C01abc4";
 const QuoterAddress = "0x82825d0554fA07f7FC52Ab63c961F330fdEFa8E8";
@@ -14,6 +17,7 @@ const ercAbi = [
   // Authenticated Functions
   "function transfer(address to, uint amount) returns (bool)",
   "function deposit() public payable",
+  "function mint(address to, uint256 amount) returns (bool)",
   "function approve(address spender, uint256 amount) returns (bool)",
 ];
 
@@ -31,7 +35,8 @@ describe("SimpleSwap", function () {
       let signer = signers[0];
       console.log("Signer balance:", await signer.getBalance());
       let weth9 = new ethers.Contract(WETH_ADDRESS, ercAbi, signer);
-      const deposit = await weth9.deposit({ value: ethers.utils.parseEther("100"), gasLimit: 500000 });
+      console.log("weth9 contract initialized");
+      const deposit = await weth9.deposit({ value: ethers.utils.parseEther("0.1"), gasLimit: 10000000 });
       await deposit.wait();
       console.log("ETH wrapped to WETH");
 
@@ -40,13 +45,17 @@ describe("SimpleSwap", function () {
       console.log(`WETH Balance: ${ethers.utils.formatEther(wethBalance)}`);
 
       console.log("Checking initial DAI balance...");
+
       const DAI = new ethers.Contract(DAI_ADDRESS, ercAbi, signer);
+      const USDC = new ethers.Contract(USDC_ADDRESS, ercAbi, signer);
+      const CELO = new ethers.Contract(CELO_ADDRESS, ercAbi, signer);
+
       const initialDaiBalance = await DAI.balanceOf(signer.address);
       const DAIBalance = Number(ethers.utils.formatUnits(initialDaiBalance, DAI_DECIMALS));
       console.log(`Initial DAI Balance: ${DAIBalance}`);
 
       console.log("Approving SimpleSwap contract to spend WETH...");
-      await weth9.approve(simpleSwap.address, ethers.utils.parseEther("100"));
+      await weth9.approve(simpleSwap.address, ethers.utils.parseEther("0.01"));
       console.log("Approval done");
 
       console.log("Checking allowance...");
@@ -54,9 +63,9 @@ describe("SimpleSwap", function () {
       console.log(`Allowance: ${ethers.utils.formatEther(allowance)}`);
 
       console.log("Executing the swap...");
-      const amountIn = ethers.utils.parseEther("10");
+      const amountIn = ethers.utils.parseEther("0.001");
       try {
-        const swap = await simpleSwap.swapWETHForDai(amountIn, { gasLimit: 500000 });
+        const swap = await simpleSwap.swapWETHForDai(amountIn, { gasLimit: 1000000 });
         await swap.wait();
         console.log("Swap executed");
       } catch (error) {
